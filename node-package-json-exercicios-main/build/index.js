ef90acf77789eff72585574a483972b8718255bc
damian-nestea <damian@hotmail.cz>
2023-05-25 18:23:10 -0300

    build criação de tsconfig e configuração de package json para início do exercício de typescript

---
+++ node-package-json-exercicios-main/build/index.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Exercício criado!");
+//# sourceMappingURL=index.js.map
--- node-package-json-exercicios-main/exercicios/process-argv.js
+++
@@ -1,6 +0,0 @@
-/* Retorna o argumento recebido via process.argv */
-console.log("Arquivo process-argv ativo!")
-
-const nome = process.argv[2]
-
-console.log(`Seu nome é ${nome}!`);
