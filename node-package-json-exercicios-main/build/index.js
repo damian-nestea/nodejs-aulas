52e73925bbe446f4d023cc853c9b43285665715e
damian-nestea <damian@hotmail.cz>
2023-05-25 18:42:44 -0300

    feat adição de types para entidades users e products, criação das entidades e impressão no console no index.ts

--- node-package-json-exercicios-main/build/index.js
+++ node-package-json-exercicios-main/build/index.js
@@ -1,3 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
 console.log("Exercício criado!");
+console.log("Usuarios:");
+console.log(database_1.users);
+console.log("___________________");
+console.log("Produtos:");
+console.log(database_1.products);
 //# sourceMappingURL=index.js.map
--- node-package-json-exercicios-main/src/index.ts
+++ node-package-json-exercicios-main/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Exercício criado!")
+import { users, products } from "./database";
+
+console.log("Exercício criado!");
+
+console.log("Usuarios:");
+console.log(users);
+
+console.log("___________________");
+console.log("Produtos:");
+console.log(products);
